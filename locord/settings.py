a19af8739959a191652228e3ad8537640cc5a85a
sw-develop <psw00719@naver.com>
2021-02-08 18:19:05 +0900

    db change

--- locord/settings.py
+++ locord/settings.py
@@ -123,12 +123,7 @@ WSGI_APPLICATION = 'locord.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = get_secret("DB_SETTINGS")
 
 
 # Password validation
