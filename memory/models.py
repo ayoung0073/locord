695e8a76abfa0dd08e3f95e91687e1310c928c8f
sw-develop <psw00719@naver.com>
2021-02-17 01:04:18 +0900

    [ADD] MODEL 빈칸 허용, 이모지

--- memory/models.py
+++ memory/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Memory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memories')
     #location
-    date = models.DateField()
-    #emoji
-    content = models.TextField()
+    date = models.DateField(null=True)
+    emoji = models.CharField(max_length=128, null=True)
+    content = models.TextField(null=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
 
     class Meta:
