1021424d073218cd650d737529656b05b0b92154
sw-develop <psw00719@naver.com>
2021-02-16 21:10:53 +0900

    [ADD] Memory

--- memory/models.py
+++ memory/models.py
@@ -1,3 +1,19 @@
+from user.models import User
+
 from django.db import models
 
-# Create your models here.
+class Memory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memories')
+    #location
+    date = models.DateField()
+    #emoji
+    content = models.TextField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+
+    class Meta:
+        db_table = 'memories'
+        verbose_name = 'Memory'
+        verbose_name_plural = 'Memories'
+        ordering = ['id']
+
+
