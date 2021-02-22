2dc7948236e5cf6445c02cdcacbae213c57b46f4
sw-develop <psw00719@naver.com>
2021-02-23 08:15:35 +0900

    [ADD]

--- memory/models.py
+++ memory/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Memory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memories')
     #location
-    date = models.DateField(null=True)
-    emoji = models.CharField(max_length=128, null=True)
-    content = models.TextField(null=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    date = models.DateField(null=True) #날짜
+    emoji = models.CharField(max_length=128, null=True) #기분
+    content = models.TextField(null=True) #기록 내용
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True) #사진
 
     class Meta:
         db_table = 'memories'
