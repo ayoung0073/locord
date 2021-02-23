1dff6a8a5c81581345fc990e3b80269a112895fd
sw-develop <psw00719@naver.com>
2021-02-23 19:44:07 +0900

    [FIX] emoji length

--- memory/models.py
+++ memory/models.py
@@ -6,7 +6,7 @@ class Memory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memories')
     #location
     date = models.DateField(null=True) #날짜
-    emoji = models.CharField(max_length=128, null=True) #기분
+    emoji = models.CharField(max_length=191, null=True) #기분
     content = models.TextField(null=True) #기록 내용
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True) #사진
 
