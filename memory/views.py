33160886041ce7b23ecff29b11839b3f9aa363a5
ayoung0073 <ayong0310@naver.com>
2021-03-03 17:52:23 +0900

    Feat : MemoryDetail GET, PUT, DELETE 인증 설정

--- memory/views.py
+++ memory/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Memory, User
 from .serializers import MemorySerializer
-#from .permissions import IsAuthenticatedOrSuperUser
 
 permission_classes = [IsAuthenticated]
 
@@ -16,7 +15,9 @@ class MemoryList(APIView):
     List all Memory or create a new Memory
     """
     def get(self, request, format=None):
-        memories = Memory.objects.all()
+        # memories = Memory.objects.all()
+        user_id = User.objects.get(email=request.user).id
+        memories = Memory.objects.filter(user_id=user_id)
         serializer = MemorySerializer(memories, many=True)
         return Response(serializer.data)
 
@@ -43,20 +44,35 @@ class MemoryDetail(APIView):
         except Memory.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
+    def check_user(self, request, pk):
         memory = self.get_object(pk)
-        serializer = MemorySerializer(memory)
-        return Response(serializer.data)
+        if memory.user_id != User.objects.get(email=request.user).id:
+            return False, None
+        request.data._mutable = True # 인증되면, request.data.user 설정
+        request.data['user'] = memory.user_id
+        request.data._mutable = False
+        return True, memory
+
+    def get(self, request, pk, format=None):
+        check = self.check_user(request, pk)
+        if not check[0]:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = MemorySerializer(check[1])
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk, format=None):
-        memory = self.get_object(pk)
-        serializer = MemorySerializer(memory, data=request.data)
+        check = self.check_user(request, pk)
+        if not check[0]:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = MemorySerializer(check[1], data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        memory = self.get_object(pk)
-        memory.delete()
+        check = self.check_user(request, pk)
+        if not check[0]:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        check[1].delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
