98bd1520182b3efd0b4ae6eb9e96ba189146004e
sw-develop <psw00719@naver.com>
2021-03-04 10:48:07 +0900

    [ADD] 위도, 경도 필드

--- memory/models.py
+++ memory/models.py
@@ -9,6 +9,8 @@ class Memory(models.Model):
     emoji = models.CharField(max_length=191, null=True) #기분
     content = models.TextField(null=True) #기록 내용
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True) #사진
+    lon = models.DecimalField(max_digits=10, decimal_places=8, null=True) #위도
+    lat = models.DecimalField(max_digits=11, decimal_places=8, null=True) #경도
 
     class Meta:
         db_table = 'memories'
