5b317e912408de9436f17a07835efbfb7bc44e04
sw-develop <psw00719@naver.com>
2021-03-04 18:39:29 +0900

    [UPDATE] viewset으로

--- memory/views.py
+++ memory/views.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from django.http import Http404
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -10,69 +12,47 @@ from .serializers import MemorySerializer
 permission_classes = [IsAuthenticated]
 
 # Create your views here.
-class MemoryList(APIView):
-    """
-    List all Memory or create a new Memory
-    """
-    def get(self, request, format=None):
-        # memories = Memory.objects.all()
+
+class MemoryViewSet(viewsets.ModelViewSet):
+    serializer_class = MemorySerializer
+    queryset = Memory.objects.all()
+
+    #각각의 사용자들의 memories 반환 & 필터링
+    @action(detail=False, url_path='user-memory') #GET 특정 사용자의 객체들
+    def user_memory(self, request):
         user_id = User.objects.get(email=request.user).id
         memories = Memory.objects.filter(user_id=user_id)
         serializer = MemorySerializer(memories, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    #post 시 설정 -> user_id 부분 넣어줘야 함
+    def create(self, request, *args, **kwargs):
         request.data._mutable = True # 임시로 mutable 변경
-        request.data['user'] = User.objects.get(email=request.user).id # 인증된 사용자 정보 저장
+        request.data['user'] = User.objects.get(email=request.user).id  # 인증된 사용자 정보 저장
         request.data._mutable = False
 
-        serializer = MemorySerializer(data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-class MemoryDetail(APIView):
     """
-    Retrieve, update or delete a Memory instance
-    """
-
-    def get_object(self, pk):
-        try:
-            return Memory.objects.get(pk=pk)
-        except Memory.DoesNotExist:
-            raise Http404
-
     def check_user(self, request, pk):
         memory = self.get_object(pk)
-        if memory.user_id != User.objects.get(email=request.user).id:
+        if memory.user_id != User.objects.get(email=request.user).id:  # 요청한 사용자와 memory의 user가 다를 때
             return False, None
-        request.data._mutable = True # 인증되면, request.data.user 설정
+        request.data._mutable = True  # 인증되면, request.data.user 설정
         request.data['user'] = memory.user_id
         request.data._mutable = False
         return True, memory
+    """
 
-    def get(self, request, pk, format=None):
-        check = self.check_user(request, pk)
-        if not check[0]:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = MemorySerializer(check[1])
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    #PATCH 특정 memory 객체 수정 partial update ?
+    def partial_update(self, request, *args, **kwargs):
+        request.data._mutable = True  # 임시로 mutable 변경
+        request.data['user'] = User.objects.get(email=request.user).id  # 인증된 사용자 정보 저장
+        request.data._mutable = False
 
-    def put(self, request, pk, format=None):
-        check = self.check_user(request, pk)
-        if not check[0]:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = MemorySerializer(check[1], data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        check = self.check_user(request, pk)
-        if not check[0]:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        check[1].delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
