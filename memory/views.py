9f2cdde3396a5a3613941f9c6b9a63bf4644c719
sw-develop <psw00719@naver.com>
2021-03-05 11:06:01 +0900

    [FIX] check user

--- memory/views.py
+++ memory/views.py
@@ -35,22 +35,25 @@ class MemoryViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    """
-    def check_user(self, request, pk):
-        memory = self.get_object(pk)
+
+    def check_user(self, request):
+        memory = self.get_object()
         if memory.user_id != User.objects.get(email=request.user).id:  # 요청한 사용자와 memory의 user가 다를 때
-            return False, None
-        request.data._mutable = True  # 인증되면, request.data.user 설정
-        request.data['user'] = memory.user_id
-        request.data._mutable = False
-        return True, memory
-    """
+            return False
+        return True
 
-    #PATCH 특정 memory 객체 수정
-    def partial_update(self, request, *args, **kwargs):
-        request.data._mutable = True  # 임시로 mutable 변경
-        request.data['user'] = User.objects.get(email=request.user).id  # 인증된 사용자 정보 저장
-        request.data._mutable = False
-
+    #PATCH
+    def partial_update(self, request, *args, **kwargs): #kwargs = pk
+        if not(self.check_user(request)) : #자신의 게시글이 아닐 경우
+            return Response({"Fail": "자신의 게시물만 수정 삭제 가능"}, status=status.HTTP_401_UNAUTHORIZED)
         kwargs['partial'] = True
         return self.update(request, *args, **kwargs)
+
+    #DELETE
+    def destroy(self, request, *args, **kwargs):
+        if not(self.check_user(request)) : #자신의 게시글이 아닐 경우
+            return Response({"Fail": "자신의 게시물만 수정 삭제 가능"}, status=status.HTTP_401_UNAUTHORIZED)
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
